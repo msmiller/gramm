396b659edfdfed8f0d185d8a2b5975e836e2b536
Mark Miller <mark.s.miller@gmail.com>
2018-09-23 20:58:20 -0700

    fix test

--- test/models.rb
+++ test/models.rb
@@ -2,12 +2,11 @@
 # @Author: Mark Miller
 # @Date:   2018-09-22 00:43:16
 # @Last Modified by:   Mark Miller
-# @Last Modified time: 2018-09-22 02:09:39
+# @Last Modified time: 2018-09-23 20:52:58
 #
 # Copyright (c) 2017-2018 Sharp Stone Codewerks / Mark S. Miller
 
-include Gramm::ActsAsGrammer
 
 class User < ActiveRecord::Base
-  acts_as_grammer
+  include Gramm::ActsAsGrammer
 end
